b5219f6e2bddb5145729a51c3221a30612552988
harsh kumar <harshku4578@gmail.com>
2022-03-19 11:58:59 +0530

    first commit

---
+++ index1.js
@@ -0,0 +1,66 @@
+var numofdrumbuttons=document.querySelectorAll(".drum").length;
+for(var i=0;i<numofdrumbuttons;i++){
+    
+    document.getElementsByTagName("button")[i].addEventListener("click",function(){
+        var buttoninnerhtml=this.innerHTML;
+        makesound(buttoninnerhtml)
+        makeanimation(buttoninnerhtml)
+
+    });
+    
+}
+
+document.addEventListener("keypress", function(event){
+    makesound(event.key)
+    makeanimation(event.key)
+    
+});
+
+
+function makesound(key){
+    switch(key){
+        case "w":
+        var tom1=new Audio("sounds/tom-1.mp3");
+        tom1.play();
+        break;
+
+        case "a":
+        var tom2=new Audio("sounds/tom-2.mp3");
+        tom2.play();
+        break;
+        
+        case "s":
+        var tom3=new Audio("sounds/tom-3.mp3");
+        tom3.play();
+        break;
+
+        case "d":
+        var tom4=new Audio("sounds/tom-4.mp3");
+        tom4.play();
+        break;
+
+        case "j":
+        var crash=new Audio("sounds/crash.mp3");
+        crash.play();
+        break;
+
+        case "k":
+        var kickbass=new Audio("sounds/kick-bass.mp3");
+        kickbass.play();
+        break;
+
+        case "l":
+        var snare=new Audio("sounds/snare.mp3");
+        snare.play();
+        break;
+    }
+}
+
+function makeanimation(currentkey){
+    var activebutton=document.querySelector("."+currentkey);
+    activebutton.classList.add("pressed");
+
+    setTimeout(function(){
+        activebutton.classList.remove("pressed")
+    },100)
+};
